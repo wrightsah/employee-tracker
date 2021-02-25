be74376daf57edc29638a1093cecbb1d15767b83
Stephen Wright <wrightsah@icloud.com>
2021-02-25 00:27:43 -0500

    set up dependencies

---
+++ index.js
@@ -0,0 +1,7 @@
+// dependencies - require mysql, inrquirer, and console.table
+
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const consoletable = require('console.table');
+
+// 
