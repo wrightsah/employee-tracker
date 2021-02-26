8c59067df9ed68c7bbe7e60e0651d3ca65f6579c
Stephen Wright <wrightsah@icloud.com>
2021-02-26 02:28:27 -0500

    created main menu

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 // dependencies - require mysql, inrquirer, and console.table
 
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-const consoletable = require('console.table');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoletable = require("console.table");
 
-//  
+// function to run questions for main menu
+
+const start = () => {
+  // inquirer questions
+
+  inquirer.prompt({
+      // "What would you like to do?"
+      name: 'mainMenu',
+      type: 'list', 
+      message: 'What would you like to do?', 
+      choices: ['Add new EMPLOYEE','Add new ROLE', 'Add new DEPARTMENT'],
+  })
+    .then((answer) => {
+        // Based on the answer, run an approprate function
+        if (answer.mainMenu === 'Add new EMPLOYEE') {
+            //run function addEmployee
+            console.log(answer.mainMenu);
+        } else if (answer.mainMenu === 'Add new ROLE') {
+            //run function addRole
+            console.log(answer.mainMenu);
+        } else if (answer.mainMenu === 'Add new DEPARTMENT') {
+            //run function addDepartment
+            console.log(answer.mainMenu);
+        }
+    });
+    // Function to add new employee
+    // What do I need when inserting a new employee?
+    // insert into [table name] (first_name, last_name, role_id, manager_id) values ('john', 'smith', etc.);
 
 
+    // Function to "Add new role"
+    // What do I need when inserting a new role?
 
-// inquirer questions
 
-// What would you like to do? 
+    // Function "Add new department"
+    // What do I need when inserting a new department?
+  };
+    
 
-// 
+
+  // Make the function go back to start
+
+start();
--- schema.sql
+++ schema.sql
@@ -6,21 +6,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 -- CREATE the table "employee"
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
     role_id int,
     manager_id int,
+    PRIMARY KEY(id)
 );
 -- CREATE the table "role"
 CREATE TABLE role (
-    id INT auto_increment PRIMARY KEY,
+    id INT auto_increment,
     title varchar(30) not null,
     salary decimal,
     department_id int,
+    PRIMARY KEY(id)
 );
 -- CREATE the table "department"
 CREATE TABLE department (
-    id INT PRIMARY KEY, 
-    name varchar(30) NOT NULL
+    id INT,
+    name varchar(30) NOT NULL,
+    PRIMARY KEY(id)
 );
